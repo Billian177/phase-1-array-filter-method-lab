b1eb627c238864bc8cef4706e87c8b07c94c1771
bill apiyo <billian177@gmail.com>
2022-06-12 14:21:10 +0300

    commit tree 20b8c8bb680f8e9007dbba2267f264b4b563afe2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching (names, string){
+    return names.filter((namesM)=> namesM.toUpperCase()===string.toUpperCase());
+}
+
+function fuzzyMatch (names, string){
+    return names.filter((namesM)=> namesM.toUpperCase().indexOf(string.toUpperCase()) ===0);
+}
+
+function matchName(driver, string){
+    return driver.filter((nameMa)=> nameMa.name===string);
+}
